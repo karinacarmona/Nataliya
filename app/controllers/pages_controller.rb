8ebaaef7fe6e73e2e09c3dfef334144cd9733b3c
Karina Carmona <karinacarmona@me.com>
2015-04-07 20:42:55 -0700

    launch heroku app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
 # before_filter :authenticate_admin!
 
   def home
+  	success_stories= SuccessStory.all
+  	@success_stories_sample = success_stories.sample(3)
   end
 
   def about
